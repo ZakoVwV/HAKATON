29cdd0a18055f2dc0faa764d49f5264b772de379
Zakov_v <amirzakov9@gmail.com>
2024-12-24 19:21:21 +0600

    change settings.py

--- config/settings.py
+++ config/settings.py
@@ -97,9 +97,11 @@ USE_TZ = True
 
 
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
